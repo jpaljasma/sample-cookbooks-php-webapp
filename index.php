adbbb3fdd2ec6fe4873e926a90ce49ee2748e24c
Jaan Paljasma <jpaljasma@gmail.com>
2013-08-27 16:44:05 -0400

    Append random number to the image URL, make test a bit less CPU intensive

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ header('Content-Type: text/html; charset=utf-8', true);
 ob_start();phpinfo();$contents = ob_get_clean();
 
 // emulate slow, cpu-intensive loop
-for($j=0;$j<100000;$j++) { $v = ''; for($i=0;$i<20;$i++) { $v=base64_encode($v.$i.''); }}
+for($j=0;$j<10000;$j++) { $v = ''; for($i=0;$i<20;$i++) { $v=base64_encode($v.$i.''); }}
 
 ?><!DOCTYPE html>
 <html>
@@ -20,6 +20,6 @@ for($j=0;$j<100000;$j++) { $v = ''; for($i=0;$i<20;$i++) { $v=base64_encode($v.$
 	<body>
 		<h1 id="testing">Load Balancer Testing</h1>
 		<h2>Server address: <?php echo $_SERVER['SERVER_ADDR']; ?></h2>
-		<p><img width="400" height="175" alt="Testing loading the image from the server" src="images/img000.jpeg" /></p>
+		<p><img width="400" height="175" alt="Testing loading the image from the server" src="images/img000.jpeg?r=<?php echo mt_rand(0,999999);?>" /></p>
 	</body>
 </html>
