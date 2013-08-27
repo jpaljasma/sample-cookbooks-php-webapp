7ce6207d8f2d994b319cd695d18bac09a4fdc0d7
Jaan Paljasma <jpaljasma@gmail.com>
2013-08-27 16:27:06 -0400

    Simple app to test HAProxy and load-based instances in OpsWorks

--- index.php
+++ index.php
@@ -6,4 +6,20 @@ ini_set('zlib.output_compression', 1024);
 ini_set('zlib.output_compression_level', 6);
 header('Content-Type: text/html; charset=utf-8', true);
 
-phpinfo();
+// run phpinfo
+ob_start();phpinfo();$contents = ob_get_clean();
+
+// emulate slow, cpu-intensive loop
+for($j=0;$j<100000;$j++) { $v = ''; for($i=0;$i<20;$i++) { $v=base64_encode($v.$i.''); }}
+
+?><!DOCTYPE html>
+<html>
+	<head>
+		<title></title>
+	</head>
+	<body>
+		<h1 id="testing">Load Balancer Testing</h1>
+		<h2>Server address: <?php echo $_SERVER['SERVER_ADDR']; ?></h2>
+		<p><img width="400" height="175" alt="Testing loading the image from the server" src="images/img000.jpeg" /></p>
+	</body>
+</html>
