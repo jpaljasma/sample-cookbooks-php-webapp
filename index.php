81ec035d138d86e5e4860fbef14188d3fe3168f6
Jaan Paljasma <jpaljasma@gmail.com>
2013-08-29 10:04:26 -0400

    Update

--- index.php
+++ index.php
@@ -13,18 +13,24 @@ for($j=0;$j<10000;$j++) { $v = ''; for($i=0;$i<20;$i++) { $v=base64_encode($v.$i
 
 $content = new stdClass();
 $content->title = 'OpsWorks Load Balancer Testing';
-$content->h1 = 'Server address:'.$_SERVER['SERVER_ADDR'];
-$content->body = '';
+$content->h1 = 'Server address: '.$_SERVER['SERVER_ADDR'];
+$content->body = '<p>This is OpsWorks theme test.</p>';
 
 /* Render the output using theme */
 if(file_exists('theme.php')) {
 	include_once('theme.php');
-	if(defined('APP_THEME')) {
+	if(defined('APP_THEME') && ('' != APP_THEME)) {
 		$themeFolder = dirname(__FILE__).'/themes/'.APP_THEME.'/';
 		$themeFile = $themeFolder.'index.php';
 		if(file_exists($themeFile)) {
 			include($themeFile);
 		}
+		else {
+			die('Theme file '.$themeFile.' does not exist.');
+		}
+	}
+	else {
+		die('APP_THEME not defined.');
 	}
 }
 else {
