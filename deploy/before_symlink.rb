6db7287acae316e47ff5212bb5149159a458b356
Jaan Paljasma <jpaljasma@gmail.com>
2013-09-12 18:26:59 -0400

    Update before_symlink.rb

--- deploy/before_symlink.rb
+++ deploy/before_symlink.rb
@@ -1,17 +1,17 @@
 current_release = release_path
 Chef::Log.info("running deploy/before_symlink.rb in folder #{current_release}")
-#Chef::Log.debug(deploy)
+Chef::Log.debug("#{node}")
 #Chef::Log.debug(node)
 
-run "echo 'release_path: #{release_path}' >> #{shared_path}/logs.log"
-run "echo 'current_path: #{current_path}' >> #{shared_path}/logs.log"
-run "echo 'shared_path: #{shared_path}' >> #{shared_path}/logs.log"
-run "echo 'node: #{node}' >> #{shared_path}/logs.log"
+#run "echo 'release_path: #{release_path}' >> #{shared_path}/logs.log"
+#run "echo 'current_path: #{current_path}' >> #{shared_path}/logs.log"
+#run "echo 'shared_path: #{shared_path}' >> #{shared_path}/logs.log"
+#run "echo 'node: #{node}' >> #{shared_path}/logs.log"
 
 #create themes directory 
 directory "#{current_release}/themes" do
   owner "apache"
-  group deploy[:group]
+  group "deploy"
   mode 0775
   action :create
 end
@@ -20,7 +20,7 @@ end
 template "#{current_release}/.htaccess" do
   source "htaccess.erb"
   mode 0644
-  group deploy[:group]
+  group "deploy"
   owner "apache"
 
   variables(
