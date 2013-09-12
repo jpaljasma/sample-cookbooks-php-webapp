f3e61b72e866e752508d295ecd5bc90794f4f093
Jaan Paljasma <jpaljasma@gmail.com>
2013-09-12 18:29:24 -0400

    Update before_symlink.rb

--- deploy/before_symlink.rb
+++ deploy/before_symlink.rb
@@ -1,6 +1,6 @@
 current_release = release_path
 Chef::Log.info("running deploy/before_symlink.rb in folder #{current_release}")
-Chef::Log.debug("#{node}")
+#Chef::Log.debug("#{node}")
 #Chef::Log.debug(node)
 
 #run "echo 'release_path: #{release_path}' >> #{shared_path}/logs.log"
@@ -9,32 +9,32 @@ Chef::Log.debug("#{node}")
 #run "echo 'node: #{node}' >> #{shared_path}/logs.log"
 
 #create themes directory 
-directory "#{current_release}/themes" do
-  owner "apache"
-  group "deploy"
-  mode 0775
-  action :create
-end
+#directory "#{current_release}/themes" do
+#  owner "apache"
+#  group "deploy"
+#  mode 0775
+#  action :create
+#end
   
 # create production .htaccess file
-template "#{current_release}/.htaccess" do
-  source "htaccess.erb"
-  mode 0644
-  group "deploy"
-  owner "apache"
+#template "#{current_release}/.htaccess" do
+#  source "htaccess.erb"
+#  mode 0644
+#  group "deploy"
+#  owner "apache"
 
-  variables(
-    :env =>    (node[:metasearch][:env] rescue nil)
-  )
-end
+#  variables(
+#    :env =>    (node[:metasearch][:env] rescue nil)
+#  )
+#end
 
 # run composer installer without dev dependencies
-script "install_composer" do
-  interpreter "bash"
-  user "root"
-  cwd current_release
-  code <<-EOH
-  curl -sS https://getcomposer.org/installer | php
-  php composer.phar install --no-dev
-  EOH
-end
+#script "install_composer" do
+#  interpreter "bash"
+#  user "root"
+#  cwd current_release
+#  code <<-EOH
+#  curl -sS https://getcomposer.org/installer | php
+#  php composer.phar install --no-dev
+#  EOH
+#end
